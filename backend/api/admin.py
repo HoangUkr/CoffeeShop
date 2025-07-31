464e8e5514f9635a8c193b47d500234570de0e35
Pham Hoang <hoangukraine@gmail.com>
2025-07-31 10:41:05 +0200

    Fix admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -16,13 +16,13 @@ class TeamAdmin(admin.ModelAdmin):
     
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('id', 'email', 'username', 'is_staff', 'is_active')
+    list_display = ('id', 'email', 'username', 'is_staff', 'role', 'is_active')
     list_filter = ('is_staff', 'is_active')
     search_fields = ('email', 'username')
     ordering = ('id',)
 
     fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
+        (None, {'fields': ('email', 'username', 'password', 'role')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
